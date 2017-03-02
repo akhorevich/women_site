6155d9340f288d8e0a767f0956217305dd3e6e79
Yulia Suhomlinova <julssuhomli@gmail.com>
2017-03-02 23:24:42 +0300

    new branch frontend added

--- site/app/gulpfile.js
+++ site/app/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var jade = require('gulp-jade');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('stylus', function () {
   return gulp.src('./build/style.styl')
@@ -14,4 +15,9 @@ gulp.task('jade', function buildHTML() {
   .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['jade', 'stylus']);
+gulp.task('imagemin', () =>
+    gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+gulp.task('default', ['jade', 'stylus', 'imagemin']);
